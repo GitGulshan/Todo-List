44c5c0775aa6770db3d09efb291acc2342182428
GitGulshan <gulshan.sahuad7@gmail.com>
2024-02-24 18:07:45 +0530

    api connect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div>Todo List</div>;
+  const [todos, settodos] = useState([]);
+  const [newTodo, setNewtodo] = useState("");
+
+  const getTodos = async () => {
+    const output = await axios.get("http://localhost:8080/getTodos");
+
+    const data = output.data;
+    settodos([...data]);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const addTodo = () => {
+    settodos([...todos, newTodo]);
+    setNewtodo("");
+  };
+
+  return (
+    <div>
+      {todos.map((todo, index) => (
+        <div key={index}>{todo.task}</div>
+      ))}
+
+      <input
+        type="text"
+        value={newTodo}
+        onInput={(e) => setNewtodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+    </div>
+  );
 }
 
 export default App;
